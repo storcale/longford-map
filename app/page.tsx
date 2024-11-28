3dfb0c15bbe1fea73cf4569b1d68bc70c945d777
Storcale <105071746+storcale@users.noreply.github.com>
2024-11-28 17:48:42 +0100

    base

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("./components/map"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
+
+export default function Home() {
+  return (
+    <main>
+      <Map />
+    </main>
+  );
+}
